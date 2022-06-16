e461a092a2bd9690fdc21045de536a0e84652325
Roberto Monjardin <rmonjardin@farmaleal.com.mx>
2022-06-16 16:51:59 -0500

    feat: :fire: avance 16/06/22

---
+++ ts-node/src/index.ts
@@ -0,0 +1,5 @@
+import { printObject, genericFunction } from "./generics/generics";
+
+//Una función genérica es una función que puede recibir cualquier tipo de argumento
+
+console.log(genericFunction(3.1416).toFixed(2));
